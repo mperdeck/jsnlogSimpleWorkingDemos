e7f7dcea576bf6c5eb719404e9a8555c4aac95c5
Matt Perdeck <matt.perdeck@readify.net>
2013-09-15 01:56:38 +1000

    initial commit

---
+++ EmptyLog4Net/App_Start/JSNLogConfig.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Web.Routing;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(
+    typeof(EmptyLog4Net.App_Start.JSNLogConfig), "PostStart")]
+
+namespace EmptyLog4Net.App_Start {
+    public static class JSNLogConfig {
+        public static void PostStart() {
+            // Insert a route at the very start of the routing table (so it gets picked up before all other routes)
+            // that ignores the jsnlog.logger route. That way, it will get through to the handler defined
+            // in web.config.
+            RouteTable.Routes.Insert(0, new Route("jsnlog.logger/{*pathInfo}", new StopRoutingHandler()));
+        }
+    }
+}
+
