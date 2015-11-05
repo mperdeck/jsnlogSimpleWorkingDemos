6fd6986475ae7f45ed35df98bf4a6c49e4e516c0
mperdeck <mperdeck@gmail.com>
2015-11-05 21:21:41 +1100

    initial owin demo project

---
+++ EmptyLog4Net - OWIN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmptyLog4Net___OWIN.MvcApplication" Language="C#" %>
---
+++ EmptyLog4Net - OWIN/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace EmptyLog4Net___OWIN
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            log4net.GlobalContext.Properties["requestId"] =
+                JSNLog.JavascriptLogging.RequestId();
+            log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
