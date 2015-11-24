364ce7acbeae6e9a4968b2eddc396d24d161f3c9
mperdeck <mperdeck@gmail.com>
2015-11-24 23:53:49 +1100

    fixing project startup issues

--- EmptyLog4Net - ConfigurationInCode/Properties/AssemblyInfo.cs
+++ EmptyLog4Net - ConfigurationInCode/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmptyLog4Net")]
+[assembly: AssemblyTitle("EmptyLog4Net___ConfigurationInCode")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("EmptyLog4Net")]
+[assembly: AssemblyProduct("EmptyLog4Net___ConfigurationInCode")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
