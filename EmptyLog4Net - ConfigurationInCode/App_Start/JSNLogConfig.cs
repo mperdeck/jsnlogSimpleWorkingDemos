e864023921f0205a715bb99ae0f0088eba157b04
mperdeck <mperdeck@gmail.com>
2015-12-13 00:15:41 +1100

    intermediate

--- EmptyLog4Net - ConfigurationInCode/App_Start/JSNLogConfig.cs
+++ EmptyLog4Net - ConfigurationInCode/App_Start/JSNLogConfig.cs
@@ -3,22 +3,21 @@ using System.Web.Routing;
 using System.Web.Mvc;
 
 [assembly: WebActivatorEx.PostApplicationStartMethod(
-    typeof(EmptyLog4Net___ConfigurationInCode.App_Start.JSNLogConfig), "PostStart")]
+    typeof(EmptyLog4Net.App_Start.JSNLogConfig), "PostStart")]
 
-namespace EmptyLog4Net___ConfigurationInCode.App_Start {
+namespace EmptyLog4Net.App_Start {
     public static class JSNLogConfig {
         public static void PostStart() {
-            // Insert a route that ignores the jsnlog.logger route. That way, 
-			// requests for jsnlog.logger will get through to the handler defined
+            // Insert a route that ignores the jsnlog.logger route. That way,
+            // requests for jsnlog.logger will get through to the handler defined
             // in web.config.
-			//
-			// The route must take this particular form, including the constraint, 
-			// otherwise ActionLink will be confused by this route and generate the wrong URLs.
-			
+            //
+            // The route must take this particular form, including the constraint,
+            // otherwise ActionLink will be confused by this route and generate the wrong URLs.
+
             var jsnlogRoute = new Route("{*jsnloglogger}", new StopRoutingHandler());
             jsnlogRoute.Constraints = new RouteValueDictionary {{ "jsnloglogger", @"jsnlog\.logger(/.*)?" }};
             RouteTable.Routes.Insert(0, jsnlogRoute);
         }
     }
 }
-
