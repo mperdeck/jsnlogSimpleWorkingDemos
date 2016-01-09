6386c3b1a867f7becf8540c3cf16f808ce22faa0
mperdeck <mperdeck@gmail.com>
2016-01-09 12:46:02 +1100

    2.15.1

--- EmptySerilog/Global.asax.cs
+++ EmptySerilog/Global.asax.cs
@@ -26,6 +26,7 @@ namespace EmptySerilog
 
             var log = new LoggerConfiguration()
                             .WriteTo.Sink(new FileSink(logFilePath, new RawFormatter(), null))
+                            .MinimumLevel.Verbose()
                             .CreateLogger();
 
             Log.Logger = log;
