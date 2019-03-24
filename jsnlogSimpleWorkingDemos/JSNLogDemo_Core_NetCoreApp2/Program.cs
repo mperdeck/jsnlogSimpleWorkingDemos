35ee570962e6e48b19763847841a1786969ed753
mperdeck@gmail.com <mperdeck@gmail.com>
2019-03-24 12:16:00 +1100

    nlog version works

--- jsnlogSimpleWorkingDemos/JSNLogDemo_Core_NetCoreApp2/Program.cs
+++ jsnlogSimpleWorkingDemos/JSNLogDemo_Core_NetCoreApp2/Program.cs
@@ -7,6 +7,12 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+/// <summary>
+/// See
+/// https://github.com/NLog/NLog.Web/wiki/Getting-started-with-ASP.NET-Core-2
+/// </summary>
 
 namespace JSNLogDemo_Core_NetCoreApp2
 {
@@ -14,13 +20,35 @@ namespace JSNLogDemo_Core_NetCoreApp2
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();  // NLog: setup NLog for Dependency injection}
     }
 }
 
--- jsnlogSimpleWorkingDemos/JSNLogDemo_Core_NetCoreApp2/Startup.cs
+++ jsnlogSimpleWorkingDemos/JSNLogDemo_Core_NetCoreApp2/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NLog.Web;
 
 using JSNLog;
 using Microsoft.Extensions.Logging;
@@ -30,12 +31,6 @@ namespace JSNLogDemo_Core_NetCoreApp2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-			// Configure the server side logging package Serilog to write to a file.
-            // JSNLog is not aware what server side logging package you use, so you can use any package you like or none at all.
-			//
-			// Note that the default level of .Net Core loggers is Information, so Trace and Debug messages are not logged.
-            loggerFactory.AddFile("Logs/log.txt");
-			
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
