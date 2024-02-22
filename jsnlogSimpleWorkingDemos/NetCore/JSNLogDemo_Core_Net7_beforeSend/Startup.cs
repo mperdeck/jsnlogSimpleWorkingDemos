75d22f1beb3eda93b6d3642b1bfcf0ed97fe1b39
mperdeck <mperdeck@gmail.com>
2024-02-22 14:16:08 +1100

    3.0.3

---
+++ jsnlogSimpleWorkingDemos/NetCore/JSNLogDemo_Core_Net7_beforeSend/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using JSNLog;
+
+
+namespace JSNLogDemo_Core_Net7_beforeSend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+			// JSNLog simply passes incoming client side log messages to the standard Net Core logging infrastructure.
+            // As a result, those messages wind up in the same logs where your server side log messages are stored. 
+			//
+			// By default, Net Core sends all log messages to the console. If you run this site by hitting F5 in Visual Studio, you will see
+			// those messages in the output window in Visual Studio. This will include the client side log messages sent by JSNLog.
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+
+            // Configure JSNLog
+			// Do this before calling UseStaticFiles.
+			//
+			// For configuration options, see
+			// https://jsnlog.com/Documentation/Configuration
+            app.UseJSNLog(loggerFactory);
+
+
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
+
+
+
+
---
+++ jsnlogSimpleWorkingDemos/NetCore/JSNLogDemo_Core_Net7_beforeSend/wwwroot/js/jsnlogdemo.js
@@ -0,0 +1,63 @@
+
+// Run this code once the page is fully loaded (including all JavaScript files)
+window.addEventListener("load", function () {
+
+
+
+      // -----------------------
+      // beforeSend example
+      //
+      // Shows a number of things you can do by setting the beforeSend option
+      // to a function.
+
+    var beforeSendExample = function (xhr, json) {
+
+        // ------------------------------------------------------------------
+        // Modify the message of the log request to the server.
+
+        json.lg[0].m += " - added by function beforeSendExample";
+
+        // ------------------------------------------------------------------
+        // Add request header
+
+        xhr.setRequestHeader('X-MyHeader', 'My header value');
+    };
+
+	// Create new appender, so you can set its beforeSend option.
+    var appender = JL.createAjaxAppender("example appender");
+    appender.setOptions({
+        "beforeSend": beforeSendExample
+    });
+
+	// Get the root logger to use the new appender.
+    JL().setOptions({
+        "appenders": [appender]
+    });
+
+    // ----- end of beforeSend example -------
+
+	
+	// Log with every severity
+	JL("jsLogger").debug("debug client log message");
+	JL("jsLogger").info("info client log message");
+	JL("jsLogger").warn({ msg: 'warn client log message - logging object', x: 5, y: 88 });
+	JL("jsLogger").error(function() { return "error client log message - returned by function"; });
+	JL("jsLogger").fatal("fatal client log message");
+
+	// Log caught exception
+	try {
+		// ReferenceError: xyz is not defined
+		xyz;
+	} catch (e) {
+		// Log the exception
+		JL().fatalException("Something went wrong!", e);
+	}
+
+	// ReferenceError: xyz2 is not defined.
+	xyz2;
+});
+
+
+
+
+
